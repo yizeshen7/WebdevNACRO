20b4f01bca5e781aa1cd3665ad676ce7be3060d5
Kieran <58400849+kieran-walsh@users.noreply.github.com>
2020-12-02 12:40:50 -0500

    Add files via upload

--- resources/scripts/scripts.js
+++ resources/scripts/scripts.js
@@ -1,9 +1,14 @@
 /* JavaScript functions for NACRO website */
 
+//Adding event listeners
 var pubSearchBar = document.querySelector('#pub-search');
 pubSearchBar.addEventListener("keyup", search);
 
+var pubLoadMore = document.querySelector('#pub-load');
+pubLoadMore.addEventListener("click", loadMorePosts);
+
 function search() {
+    loadMorePosts(); //Load all the results to search all the results
     var cardTitles = document.getElementsByClassName('card-title');
     var cardTexts = document.getElementsByClassName('card-text');
     var cards = document.getElementsByClassName('card');
@@ -18,6 +23,17 @@ function search() {
         }
         else {
             cards[i].style.display = "none";
+        }
+    }
+}
+
+function loadMorePosts() {
+    pubLoadMore.removeEventListener("click", loadMorePosts);
+    pubLoadMore.style.display = "none";
+    var cards = document.getElementsByClassName('card');
+    for (var i = 0; cards.length; i++) {
+        if (cards[i].style.display == "none") {
+            cards[i].style.display = "block";
         }
     }
 }
